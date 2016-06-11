713892e531052a08e91ecffcbebe2ed35391d669
Ipsis <ipsis418@gmail.com>
2016-06-11 17:02:28 +0100

    Add fake player without the looting enchant - NO ONE SAW ANYTHING [#47]
    With the three fake players with the looting swords it works perfectly,
    until you have a factory without looting. In which case you cannot
    get a fake player to do the mob kill and the factory never learns
    any mob drops.
    
    I'll get my coat.

--- src/main/java/ipsis/woot/util/FakePlayerPool.java
+++ src/main/java/ipsis/woot/util/FakePlayerPool.java
@@ -16,9 +16,10 @@ import java.util.UUID;
 
 public class FakePlayerPool {
 
-    static GameProfile WOOT_GAME_PROFILE_I = new GameProfile(UUID.nameUUIDFromBytes("[Woot_I]".getBytes()), "[Woot]");
-    static GameProfile WOOT_GAME_PROFILE_II = new GameProfile(UUID.nameUUIDFromBytes("[Woot_II]".getBytes()), "[Woot]");
-    static GameProfile WOOT_GAME_PROFILE_III = new GameProfile(UUID.nameUUIDFromBytes("[Woot_III]".getBytes()), "[Woot]");
+    static GameProfile WOOT_GAME_PROFILE = new GameProfile(UUID.nameUUIDFromBytes("[Woot]".getBytes()), "[Woot]");
+    static GameProfile WOOT_GAME_PROFILE_I = new GameProfile(UUID.nameUUIDFromBytes("[Woot_I]".getBytes()), "[Woot I]");
+    static GameProfile WOOT_GAME_PROFILE_II = new GameProfile(UUID.nameUUIDFromBytes("[Woot_II]".getBytes()), "[Woot II]");
+    static GameProfile WOOT_GAME_PROFILE_III = new GameProfile(UUID.nameUUIDFromBytes("[Woot_III]".getBytes()), "[Woot III]");
 
     static HashMap<EnumEnchantKey, FakePlayer> fakePlayerHashMap;
 
@@ -26,9 +27,11 @@ public class FakePlayerPool {
 
         fakePlayerHashMap  = new HashMap<EnumEnchantKey, FakePlayer>();
 
+        FakePlayer fakePlayer = FakePlayerFactory.get(world, WOOT_GAME_PROFILE);
         FakePlayer fakePlayerI = FakePlayerFactory.get(world, WOOT_GAME_PROFILE_I);
         FakePlayer fakePlayerII = FakePlayerFactory.get(world, WOOT_GAME_PROFILE_II);
         FakePlayer fakePlayerIII = FakePlayerFactory.get(world, WOOT_GAME_PROFILE_III);
+        ItemStack sword = new ItemStack(Items.DIAMOND_SWORD);
         ItemStack swordI = new ItemStack(Items.DIAMOND_SWORD);
         ItemStack swordII = new ItemStack(Items.DIAMOND_SWORD);
         ItemStack swordIII = new ItemStack(Items.DIAMOND_SWORD);
@@ -37,10 +40,12 @@ public class FakePlayerPool {
         swordII.addEnchantment(Enchantment.getEnchantmentByLocation("looting"), Settings.lootingIIILevel);
         swordIII.addEnchantment(Enchantment.getEnchantmentByLocation("looting"), Settings.lootingIIILevel);
 
+        fakePlayer.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, sword);
         fakePlayerI.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, swordI);
         fakePlayerII.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, swordII);
         fakePlayerIII.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, swordIII);
 
+        fakePlayerHashMap.put(EnumEnchantKey.NO_ENCHANT, fakePlayer);
         fakePlayerHashMap.put(EnumEnchantKey.LOOTING_I, fakePlayerI);
         fakePlayerHashMap.put(EnumEnchantKey.LOOTING_II, fakePlayerII);
         fakePlayerHashMap.put(EnumEnchantKey.LOOTING_III, fakePlayerIII);

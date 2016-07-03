9e7da8ef81b68af0a9f9205cb162f9390311e64a
Ipsis <ipsis418@gmail.com>
2016-07-03 18:51:34 +0100

    Actually add the prismframe

---
+++ src/main/java/ipsis/woot/item/ItemPrismFrame.java
@@ -0,0 +1,25 @@
+package ipsis.woot.item;
+
+import ipsis.woot.init.ModItems;
+import ipsis.woot.oss.client.ModelHelper;
+import ipsis.woot.reference.Reference;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemPrismFrame extends ItemWoot {
+
+    public static final String BASENAME = "prismFrame";
+
+    public ItemPrismFrame() {
+
+        super(BASENAME);
+        setRegistryName(Reference.MOD_ID_LOWER, BASENAME);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void initModel() {
+
+        ModelHelper.registerItem(ModItems.itemPrismFrame, BASENAME.toLowerCase());
+    }
+}

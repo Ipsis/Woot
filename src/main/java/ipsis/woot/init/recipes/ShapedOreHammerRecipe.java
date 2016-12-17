7696a7c0d3644d5089707660b5c5b940494abfe8
Ipsis <ipsis418@gmail.com>
2016-12-17 11:48:58 +0000

    Fix ShapedOreRecipe.getRemainingItems returning a NonNullList<ItemStack>

--- src/main/java/ipsis/woot/init/recipes/ShapedOreHammerRecipe.java
+++ src/main/java/ipsis/woot/init/recipes/ShapedOreHammerRecipe.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockObsidian;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.oredict.RecipeSorter;
 import net.minecraftforge.oredict.ShapedOreRecipe;
@@ -28,18 +29,18 @@ public class ShapedOreHammerRecipe extends ShapedOreRecipe {
     }
 
     @Override
-    public ItemStack[] getRemainingItems(InventoryCrafting inv) {
+    public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
 
-        ItemStack[] ret = new ItemStack[inv.getSizeInventory()];
-        for (int i = 0; i < ret.length; i++) {
+        NonNullList<ItemStack> ret = NonNullList.withSize(inv.getSizeInventory(), ItemStack.EMPTY);
+        for (int i = 0; i < ret.size(); i++) {
             ItemStack itemStack = inv.getStackInSlot(i);
 
-            if (itemStack != null && itemStack.getItem() instanceof ItemYahHammer)
-                ret[i] = ItemHandlerHelper.copyStackWithSize(itemStack, 1);
-            else if (itemStack != null && Block.getBlockFromItem(itemStack.getItem()) instanceof BlockObsidian)
-                ret[i] = ItemHandlerHelper.copyStackWithSize(itemStack, 1);
-            else if (itemStack != null && itemStack.getItem() instanceof ItemDye)
-                ret[i] = ItemHandlerHelper.copyStackWithSize(itemStack, 1);
+            if (!itemStack.isEmpty() && itemStack.getItem() instanceof ItemYahHammer)
+                ret.set(i, ItemHandlerHelper.copyStackWithSize(itemStack, 1));
+            else if (!itemStack.isEmpty() && Block.getBlockFromItem(itemStack.getItem()) instanceof BlockObsidian)
+                ret.set(i, ItemHandlerHelper.copyStackWithSize(itemStack, 1));
+            else if (!itemStack.isEmpty() && itemStack.getItem() instanceof ItemDye)
+                ret.set(i, ItemHandlerHelper.copyStackWithSize(itemStack, 1));
         }
         return ret;
     }

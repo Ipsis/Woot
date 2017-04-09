fb0e8af58c4c141c43c51489ea7f9fa7f8925124
Ipsis <ipsis418@gmail.com>
2017-04-09 09:15:06 +0100

    First version of the extra spawn requirements

---
+++ src/main/java/ipsis/woot/manager/spawnreq/ExtraSpawnReq.java
@@ -0,0 +1,156 @@
+package ipsis.woot.manager.spawnreq;
+
+import com.google.gson.*;
+import ipsis.woot.manager.EnumEnchantKey;
+import ipsis.woot.manager.MobRegistry;
+import ipsis.woot.oss.LogHelper;
+import ipsis.woot.util.ItemStackHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * For now items have precedence over fluid
+ * Fluid can only be one fluid
+ */
+public class ExtraSpawnReq {
+
+    protected String wootName;
+    protected EnumEnchantKey enchantKey;
+    protected List<ItemStack> items;
+    protected List<FluidStack> fluids;
+
+    public ExtraSpawnReq() {
+
+        wootName = MobRegistry.INVALID_MOB_NAME;
+        enchantKey = EnumEnchantKey.NO_ENCHANT;
+        items = new ArrayList<ItemStack>();
+        fluids = new ArrayList<FluidStack>();
+    }
+
+
+    public boolean hasItems() { return !items.isEmpty(); }
+    public boolean hasFluids() { return !hasItems() && !fluids.isEmpty(); }
+
+    public void setWootName(String wootName) {
+        this.wootName = wootName;
+    }
+
+    public String getWootName() { return this.wootName; }
+    public EnumEnchantKey getKey() { return this.enchantKey; }
+
+    public void setKey(EnumEnchantKey enchantKey) {
+        this.enchantKey = enchantKey;
+    }
+
+    public void addItemStack(String name, int stackSize) {
+
+        ItemStack itemStack = ItemStackHelper.getItemStackFromName(name);
+        if (itemStack != null) {
+            itemStack.stackSize = stackSize;
+            if (itemStack.stackSize > itemStack.getMaxStackSize())
+                itemStack.stackSize = itemStack.getMaxStackSize();
+            items.add(itemStack);
+        } else {
+            LogHelper.info("ExtraSpawnReq: Failed to convert " + name);
+        }
+    }
+
+    public void addFluidStack(String name, int amount) {
+
+        FluidStack fluidStack = FluidRegistry.getFluidStack(name, amount);
+        if (fluidStack != null) {
+            fluids.add(fluidStack);
+        } else {
+            LogHelper.info("ExtraSpawnReq: Failed to convert " + name);
+        }
+    }
+
+    public List<ItemStack> getItems() {
+        return items;
+    }
+
+    public List<FluidStack> getFluids() {
+
+        if (!hasItems())
+            return fluids;
+        else
+            return new ArrayList<>();
+    }
+
+    public static class ItemStackSerializer implements JsonSerializer<ItemStack> {
+
+        @Override
+        public JsonElement serialize(ItemStack src, Type typeOfSrc, JsonSerializationContext context) {
+
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("item", ItemStackHelper.getItemStackName(src));
+            jsonObject.addProperty("amount", src.stackSize);
+            return jsonObject;
+        }
+    }
+
+    public static class FluidStackSerializer implements JsonSerializer<FluidStack> {
+
+        @Override
+        public JsonElement serialize(FluidStack src, Type typeOfSrc, JsonSerializationContext context) {
+
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("fluid", FluidRegistry.getFluidName(src.getFluid()));
+            jsonObject.addProperty("amount", src.amount);
+            return jsonObject;
+        }
+    }
+
+    public static class Serializer implements JsonDeserializer<ExtraSpawnReq>, JsonSerializer<ExtraSpawnReq> {
+
+        @Override
+        public ExtraSpawnReq deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+            JsonObject jsonObject = json.getAsJsonObject();
+            String name = JsonUtils.getString(jsonObject, "mob");
+            String key = JsonUtils.getString(jsonObject, "enchant");
+            JsonArray jsonArray = JsonUtils.getJsonArray(jsonObject, "items");
+
+            ExtraSpawnReq req = new ExtraSpawnReq();
+            req.setKey(EnumEnchantKey.getFromString(key));
+            req.setWootName(name);
+
+            for (int i = 0; i < jsonArray.size(); i++ ) {
+                JsonElement jsonElement = jsonArray.get(i);
+                String itemName = JsonUtils.getString(jsonElement.getAsJsonObject(), "item");
+                int amount = JsonUtils.getInt(jsonElement.getAsJsonObject(), "amount");
+                req.addItemStack(itemName, amount);
+            }
+
+            jsonArray = JsonUtils.getJsonArray(jsonObject, "fluids");
+            for (int i = 0; i < jsonArray.size(); i++ ) {
+                JsonElement jsonElement = jsonArray.get(i);
+                String fluidName = JsonUtils.getString(jsonElement.getAsJsonObject(), "fluid");
+                int amount = JsonUtils.getInt(jsonElement.getAsJsonObject(), "amount");
+                req.addFluidStack(fluidName, amount);
+            }
+
+            return req;
+        }
+
+        @Override
+        public JsonElement serialize(ExtraSpawnReq src, Type typeOfSrc, JsonSerializationContext context) {
+
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("mob", src.getWootName());
+            jsonObject.addProperty("enchant", src.getKey().toString());
+            jsonObject.add("items", context.serialize(src.items));
+            jsonObject.add("fluids", context.serialize(src.fluids));
+
+            return jsonObject;
+        }
+    }
+
+}
+
--- src/main/java/ipsis/woot/reference/Files.java
+++ src/main/java/ipsis/woot/reference/Files.java
@@ -8,8 +8,10 @@ public class Files {
 
     public static File globalDataDirectory;
     public static File lootFile;
+    public static File spawnReqFile;
 
     private static final String LOOT_FILENAME = "loot.json";
+    private static final String SPAWN_REQ_FILENAME = "spawnreq.json";
 
     public static void init(FMLPreInitializationEvent event) {
 
@@ -17,6 +19,7 @@ public class Files {
                 File.separator + Reference.MOD_NAME_LOWER);
 
         lootFile = new File(globalDataDirectory, LOOT_FILENAME);
+        spawnReqFile = new File(globalDataDirectory, SPAWN_REQ_FILENAME);
     }
 
     public static String getWootFileForDisplay() {
@@ -26,6 +29,12 @@ public class Files {
          * As that is what FMLLog is using to display this
          */
         String raw = lootFile.toString();
+        return raw.replace("%", "%%");
+    }
+
+    public static String getWootSpawnReqFileForDisplay() {
+
+        String raw = spawnReqFile.toString();
         return raw.replace("%", "%%");
     }
 }

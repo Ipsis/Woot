bbfac9774e6d722db037ebd91badcf9a01061a3d
Ipsis <ipsis418@gmail.com>
2017-07-06 21:24:09 +0100

    Bump to Forge 14.21.0.2385 and latest top

--- src/main/java/ipsis/woot/client/GuiFactory.java
+++ src/main/java/ipsis/woot/client/GuiFactory.java
@@ -24,19 +24,7 @@ public class GuiFactory implements IModGuiFactory {
     }
 
     @Override
-    public Class<? extends GuiScreen> mainConfigGuiClass() {
-
-        return ModGuiConfig.class;
-    }
-
-    @Override
     public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
-
-        return null;
-    }
-
-    @Override
-    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
 
         return null;
     }

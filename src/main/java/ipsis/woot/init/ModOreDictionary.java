b4cf0c7002af8b767b38a9b88fd59514082fc4a8
Ipsis <ipsis418@gmail.com>
2017-09-24 10:20:22 +0100

    No longer register oredict for skulls

--- src/main/java/ipsis/woot/init/ModOreDictionary.java
+++ src/main/java/ipsis/woot/init/ModOreDictionary.java
@@ -1,20 +1,8 @@
 package ipsis.woot.init;
 
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
-
 public class ModOreDictionary {
-
-    public static final String ORE_DICT_SKULL = "itemSkull";
 
     public static void init() {
 
-        /**
-         * Register the skulls under "itemSkull" which matches up with EnderIO and will pickup
-         * the Enderman skulls
-         */
-        ItemStack skull = new ItemStack(Items.SKULL, 1, OreDictionary.WILDCARD_VALUE);
-        OreDictionary.registerOre(ORE_DICT_SKULL, skull);
     }
 }

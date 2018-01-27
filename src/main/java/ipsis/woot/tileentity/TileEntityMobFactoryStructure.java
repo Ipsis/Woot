2b0edf716a354e7cde78dc8b4104c6bb9740cc03
Ipsis <ipsis418@gmail.com>
2018-01-27 12:57:21 +0000

    Add new master locator which walks over blocks, not TEs

--- src/main/java/ipsis/woot/tileentity/TileEntityMobFactoryStructure.java
+++ src/main/java/ipsis/woot/tileentity/TileEntityMobFactoryStructure.java
@@ -1,9 +1,6 @@
 package ipsis.woot.tileentity;
 
-import ipsis.woot.farmblocks.IFarmBlockConnection;
-import ipsis.woot.farmblocks.IFarmBlockMaster;
-import ipsis.woot.farmblocks.IFarmBlockStructure;
-import ipsis.woot.farmblocks.StructureMasterLocator;
+import ipsis.woot.farmblocks.*;
 import ipsis.woot.util.WorldHelper;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
@@ -34,8 +31,12 @@ public class TileEntityMobFactoryStructure extends TileEntity implements IFarmBl
     public void validate() {
 
         super.validate();
-        if (!getWorld().isRemote)
-            blockAdded();
+        if (!getWorld().isRemote) {
+            // This MUST use the non-te version
+            IFarmBlockMaster tmpMaster = new StructureBlockMasterLocator().findMaster(getWorld(), getPos());
+            if (tmpMaster != null)
+                tmpMaster.interruptFarmStructure();
+        }
     }
 
     @Override

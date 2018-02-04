37dc44eb459d4563cd05fa0065f3cd2651eb3a66
Ipsis <ipsis418@gmail.com>
2018-02-04 10:42:53 +0000

    Move from BlankModPlugin->IModPlugin for JEI

--- src/main/java/ipsis/woot/plugins/jei/JEIPlugin.java
+++ src/main/java/ipsis/woot/plugins/jei/JEIPlugin.java
@@ -5,18 +5,17 @@ import ipsis.woot.plugins.jei.anvil.StygianAnvilRecipeCategory;
 import ipsis.woot.plugins.jei.anvil.StygianAnvilRecipeHandler;
 import ipsis.woot.plugins.jei.anvil.StygianAnvilRecipeMaker;
 import ipsis.woot.reference.Reference;
-import mezz.jei.api.BlankModPlugin;
 import mezz.jei.api.IJeiHelpers;
+import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import net.minecraft.item.ItemStack;
 
 @mezz.jei.api.JEIPlugin
-public class JEIPlugin extends BlankModPlugin {
+public class JEIPlugin implements IModPlugin {
 
     public static final String JEI_ANVIL = Reference.MOD_ID + ":stygianavil";
 
     public static IJeiHelpers jeiHelper;
-
     @Override
     public void register(IModRegistry registry) {
 
@@ -27,6 +26,5 @@ public class JEIPlugin extends BlankModPlugin {
         registry.addRecipes(StygianAnvilRecipeMaker.getRecipes());
         registry.addRecipeCategoryCraftingItem(new ItemStack(ModBlocks.blockAnvil), JEI_ANVIL);
         registry.addDescription(new ItemStack(ModBlocks.blockAnvil), "Right-click with a die or Ender Shard to place it on the anvil. Drop the other ingredients on the anvil, then hit with the Yah Hammer");
-
     }
 }

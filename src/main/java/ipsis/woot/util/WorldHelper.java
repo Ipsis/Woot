616026e048fc06bcb7b5f1cc8a93097da89c633e
Ipsis <ipsis418@gmail.com>
2018-04-08 10:08:51 +0100

    Respect maxStackSize when spawning item in world

--- src/main/java/ipsis/woot/util/WorldHelper.java
+++ src/main/java/ipsis/woot/util/WorldHelper.java
@@ -2,6 +2,8 @@ package ipsis.woot.util;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -20,6 +22,22 @@ public class WorldHelper {
         if (world != null) {
             IBlockState iBlockState = world.getBlockState(pos);
             world.notifyNeighborsOfStateChange(pos, b, false);
+        }
+    }
+
+    public static void spawnInWorld(World world, BlockPos pos, ItemStack itemStack) {
+
+        if (itemStack.getCount() > itemStack.getMaxStackSize()) {
+            for (int i = 0; i < itemStack.getCount(); i++) {
+                ItemStack outStack = itemStack.copy();
+                outStack.setCount(1);
+                EntityItem out = new EntityItem(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, outStack);
+                world.spawnEntity(out);
+            }
+        } else {
+            ItemStack outStack = itemStack.copy();
+            EntityItem out = new EntityItem(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, outStack);
+            world.spawnEntity(out);
         }
     }
 }

a6b9fb9e60ef6803dcd8f6d6615dcc5d817cf432
Ipsis <ipsis418@gmail.com>
2018-05-03 15:38:09 +0100

    [----] Add EnderIO Vat recipe for XpShard->XpJuice

--- src/main/java/ipsis/woot/plugins/imc/EnderIO.java
+++ src/main/java/ipsis/woot/plugins/imc/EnderIO.java
@@ -4,8 +4,7 @@ import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 public class EnderIO {
 
-    static final String VAT_RECIPE = "recipe:vat";
-    static final String ENDERIO_MODID = "EnderIO";
+    static final String ENDERIO_MODID = "enderio";
 
     public static void loadRecipes() {
 
@@ -15,20 +14,25 @@ public class EnderIO {
          * experience to liquid = xp * 20
          * => 16 * 20 = 320mB
          */
-        String recipe =
-                "<recipeGroup name=\"Woot\" >" +
-                        "<recipe name=\"Liquid XP\" energyCost=\"20\" >" +
-                        "<input>" +
-                        "<itemStack modID=\"Woot\" itemName=\"xpShard\" slot=\"0\" />" +
-                        "<fluidStack name=\"water\" multiplier=\"0.32\" />" +
-                        "</input>" +
-                        "<output>" +
-                        "<fluidStack name=\"xpjuice\" />" +
-                        "</output>" +
-                        "</recipe>" +
-                        "</recipeGroup>";
-
-        FMLInterModComms.sendMessage(ENDERIO_MODID, VAT_RECIPE, recipe);
+        String s = String.format(XML_MESSAGE_VAT, "Liquid XP", 20, "woot:xpshard", 1.0, "water", 0.32, "xpjuice");
+        FMLInterModComms.sendMessage(ENDERIO_MODID, "recipe:xml", s);
     }
+
+    private static final String XML_MESSAGE_VAT = "" +
+            "<recipes>" +
+                "<recipe name=\"Vat: %s\" required=\"true\" disabled=\"false\">" +
+                    "<fermenting energy=\"%d\">" +
+                        "<inputgroup>" +
+                            "<input name=\"%s\" multiplier=\"%f\"/>" +
+                        "</inputgroup>" +
+                        "<inputfluid name=\"%s\" multiplier=\"%f\"/>" +
+                        "<outputfluid name=\"%s\"/>" +
+                    "</fermenting>" +
+                "</recipe>" +
+            "</recipes>";
+
+
+
+
 
 }

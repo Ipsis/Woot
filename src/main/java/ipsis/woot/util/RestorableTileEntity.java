ab35f2a5934c97ea3cb29ac2f353d41749e887f0
Ipsis <ipsis418@gmail.com>
2019-07-25 19:36:28 +0100

    forge: 28.0.14

--- src/main/java/ipsis/woot/util/RestorableTileEntity.java
+++
@@ -1,8 +0,0 @@
-package ipsis.woot.util;
-
-import net.minecraft.nbt.CompoundNBT;
-
-public interface RestorableTileEntity {
-    void writeRestorableToNBT(CompoundNBT compoundNBT);
-    void readRestorableFromNBT(CompoundNBT compoundNBT);
-}

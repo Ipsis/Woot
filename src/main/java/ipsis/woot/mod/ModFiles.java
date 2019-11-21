911013227fd748907b938b24e37c182200067312
Ipsis <ipsis418@gmail.com>
2019-11-21 09:08:21 +0000

    Forgot the ModFiles file

---
+++ src/main/java/ipsis/woot/mod/ModFiles.java
@@ -0,0 +1,25 @@
+package ipsis.woot.mod;
+
+import ipsis.woot.Woot;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ModFiles {
+
+    public static ModFiles INSTANCE = new ModFiles();
+
+    private Path lootFile;
+
+    public void init() {
+        this.lootFile = FMLPaths.GAMEDIR.get().resolve(
+                Paths.get(Woot.MODID, "loot.json"));
+        Woot.LOGGER.error(lootFile);
+    }
+
+    public File getLootFile() {
+        return this.lootFile.toFile();
+    }
+}

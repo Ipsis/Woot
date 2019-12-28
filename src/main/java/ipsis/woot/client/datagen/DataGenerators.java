129f7506c411279a4da755e80b25fe486dea4e1b
Ipsis <ipsis418@gmail.com>
2019-12-28 07:27:11 +0000

    Add recipe data generators

---
+++ src/main/java/ipsis/woot/client/datagen/DataGenerators.java
@@ -0,0 +1,21 @@
+package ipsis.woot.client.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        if (event.includeServer()) {
+            generator.addProvider(new Recipes(generator));
+        }
+        if (event.includeClient()) {
+
+        }
+    }
+}
---
+++ src/main/java/ipsis/woot/client/datagen/Recipes.java
@@ -0,0 +1,38 @@
+package ipsis.woot.client.datagen;
+
+import ipsis.woot.Woot;
+import ipsis.woot.mod.ModBlocks;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraftforge.common.Tags;
+
+import java.util.function.Consumer;
+
+public class Recipes extends RecipeProvider {
+
+    public Recipes(DataGenerator generator) {
+        super(generator);
+    }
+
+    @Override
+    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(ModBlocks.LAYOUT_BLOCK)
+                .patternLine("grg")
+                .patternLine("ygb")
+                .patternLine("gwg")
+                .key('g', Tags.Items.GLASS)
+                .key('r', Tags.Items.DYES_RED)
+                .key('y', Tags.Items.DYES_YELLOW)
+                .key('b', Tags.Items.DYES_BLACK)
+                .key('w', Tags.Items.DYES_WHITE)
+                .setGroup(Woot.MODID)
+                .addCriterion("cobblestone",
+                        InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE))
+                .build(consumer);
+
+    }
+}

6981ad80ad18f122775680beff4d2d9c0d01dd4c
Ipsis <ipsis418@gmail.com>
2020-04-24 11:16:44 +0100

    cell: support bucket in [WOOT-41]

--- src/main/java/ipsis/woot/modules/factory/blocks/CellBlock.java
+++ src/main/java/ipsis/woot/modules/factory/blocks/CellBlock.java
@@ -5,12 +5,14 @@ import ipsis.woot.modules.factory.FactoryComponent;
 import ipsis.woot.modules.factory.FactoryComponentProvider;
 import ipsis.woot.modules.factory.FactoryConfiguration;
 import ipsis.woot.modules.factory.FactorySetup;
+import ipsis.woot.modules.infuser.blocks.InfuserTileEntity;
 import ipsis.woot.util.WootDebug;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
@@ -18,12 +20,18 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -55,6 +63,22 @@ public class CellBlock extends Block implements WootDebug, FactoryComponentProvi
         } catch (InstantiationException | IllegalAccessException e) {
             throw new RuntimeException();
         }
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult p_225533_6_) {
+        if (worldIn.isRemote)
+            return ActionResultType.SUCCESS;
+
+        if (!(worldIn.getTileEntity(pos) instanceof CellTileEntityBase))
+            throw new IllegalStateException("Tile entity is missing");
+
+
+        ItemStack heldItem = player.getHeldItem(handIn);
+        if (FluidUtil.getFluidHandler(heldItem).isPresent())
+            return FluidUtil.interactWithFluidHandler(player, handIn, worldIn, pos, null) ? ActionResultType.SUCCESS : ActionResultType.FAIL;
+
+        return ActionResultType.SUCCESS;
     }
 
     @OnlyIn(Dist.CLIENT)

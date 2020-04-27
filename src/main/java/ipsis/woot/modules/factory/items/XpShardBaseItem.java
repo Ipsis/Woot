98788a401e598ba9dff43120195ac77857be78a6
Ipsis <ipsis418@gmail.com>
2020-04-27 10:04:59 +0100

    shard: click to gain experience

--- src/main/java/ipsis/woot/modules/factory/items/XpShardBaseItem.java
+++ src/main/java/ipsis/woot/modules/factory/items/XpShardBaseItem.java
@@ -2,9 +2,21 @@ package ipsis.woot.modules.factory.items;
 
 import ipsis.woot.Woot;
 import ipsis.woot.modules.factory.FactorySetup;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.item.ExperienceOrbEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.util.FakePlayer;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +33,8 @@ public class XpShardBaseItem extends Item {
 
     private static final int STACK_SIZE = 64;
     public static final int SPLINTERS_IN_STACK = 9;
+    private static final int SHARD_XP = 9;
+    private static final int SPLINTER_XP = 1;
 
     final Variant variant;
     public XpShardBaseItem(Variant variant) {
@@ -71,5 +85,68 @@ public class XpShardBaseItem extends Item {
         }
 
         return shards;
+    }
+
+    private int getXp(ItemStack itemStack) {
+        if (itemStack.getItem() == FactorySetup.XP_SHARD_ITEM.get())
+            return SHARD_XP;
+        else if (itemStack.getItem() == FactorySetup.XP_SPLINTER_ITEM.get())
+            return SPLINTER_XP;
+        return 0;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+
+        if (worldIn.isRemote)
+            return ActionResult.resultPass(playerIn.getHeldItem(handIn));
+
+        ItemStack itemStack = playerIn.getHeldItem(handIn);
+        if (itemStack.isEmpty())
+            return ActionResult.resultPass(playerIn.getHeldItem(handIn));
+
+        worldIn.playSound(
+                null,
+                playerIn.getPosX(),
+                playerIn.getPosY(),
+                playerIn.getPosZ(),
+                SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP,
+                SoundCategory.PLAYERS,
+                0.2F,
+                0.5F * ((random.nextFloat() - random.nextFloat()) * 0.7F + 1.8F));
+
+        if (playerIn instanceof FakePlayer) {
+            // Fake player can only use one at a time
+            worldIn.addEntity(new ExperienceOrbEntity(
+                            worldIn,
+                            playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(),
+                            1));
+            itemStack.shrink(1);
+        } else {
+            int xp = 0;
+            if (playerIn.isCrouching()) {
+                // Consume the whole stack
+                xp = getXp(itemStack) * itemStack.getCount();
+                if (!playerIn.isCreative())
+                    itemStack.setCount(0);
+            } else {
+                xp = getXp(itemStack);
+                if (!playerIn.isCreative())
+                    itemStack.shrink(1);
+            }
+            if (xp > 0) {
+                playerIn.giveExperiencePoints(xp);
+            }
+        }
+        return ActionResult.resultSuccess(itemStack);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+
+        tooltip.add(new TranslationTextComponent("info.woot.shard.0"));
+        tooltip.add(new TranslationTextComponent("info.woot.shard.1"));
     }
 }

7ef594411fa9ba2216ff7139e1cc5a3ab3e6487a
Ipsis <ipsis418@gmail.com>
2020-05-02 10:54:45 +0100

    layout: add torch to the recipe [WOOT-2]

--- src/main/java/ipsis/woot/datagen/modules/Layout.java
+++ src/main/java/ipsis/woot/datagen/modules/Layout.java
@@ -6,6 +6,7 @@ import net.minecraft.advancements.criterion.InventoryChangeTrigger;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.item.Items;
 import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
@@ -15,13 +16,14 @@ public class Layout {
 
         ShapedRecipeBuilder.shapedRecipe(LayoutSetup.LAYOUT_BLOCK.get())
                 .patternLine("grg")
-                .patternLine("ygb")
+                .patternLine("ytb")
                 .patternLine("gwg")
                 .key('g', Tags.Items.GLASS)
                 .key('r', Tags.Items.DYES_RED)
                 .key('y', Tags.Items.DYES_YELLOW)
                 .key('b', Tags.Items.DYES_BLACK)
                 .key('w', Tags.Items.DYES_WHITE)
+                .key('t', Items.TORCH)
                 .setGroup(Woot.MODID)
                 .addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE))
                 .build(consumer);

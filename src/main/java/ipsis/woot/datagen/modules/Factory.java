5f9cad340669d2667e7c6f63e5125e6d898df705
Ipsis <ipsis418@gmail.com>
2020-05-03 12:28:47 +0100

    recipes: all recipes now present

--- src/main/java/ipsis/woot/datagen/modules/Factory.java
+++ src/main/java/ipsis/woot/datagen/modules/Factory.java
@@ -4,6 +4,7 @@ import ipsis.woot.Woot;
 import ipsis.woot.mod.ModTags;
 import ipsis.woot.modules.anvil.AnvilSetup;
 import ipsis.woot.modules.factory.FactorySetup;
+import ipsis.woot.modules.factory.items.PerkItem;
 import ipsis.woot.modules.fluidconvertor.FluidConvertorSetup;
 import ipsis.woot.modules.generic.GenericSetup;
 import ipsis.woot.modules.generic.items.GenericItem;
@@ -14,8 +15,9 @@ import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.ShapedRecipeBuilder;
 import net.minecraft.data.ShapelessRecipeBuilder;
 import net.minecraft.item.Items;
-import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.IItemProvider;
 import net.minecraftforge.common.Tags;
+import net.minecraftforge.fml.RegistryObject;
 
 import java.util.function.Consumer;
 
@@ -180,6 +182,97 @@ public class Factory {
                 .setGroup(Woot.MODID)
                 .addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE))
                 .build(consumer);
+
+        /**
+         * Perks
+         */
+        class RecipePerk {
+            RegistryObject<PerkItem> perkItem1;
+            RegistryObject<PerkItem> perkItem2;
+            RegistryObject<PerkItem> perkItem3;
+            IItemProvider i1;
+            IItemProvider i2;
+            public RecipePerk(RegistryObject<PerkItem> perkItem1, RegistryObject<PerkItem> perkItem2,
+                    RegistryObject<PerkItem> perkItem3, IItemProvider i1, IItemProvider i2) {
+                this.perkItem1 = perkItem1;
+                this.perkItem2 = perkItem2;
+                this.perkItem3 = perkItem3;
+                this.i1 = i1;
+                this.i2 = i2;
+            }
+
+        }
+        RecipePerk[] recipePerks = {
+                new RecipePerk(
+                        FactorySetup.EFFICIENCY_1_ITEM,
+                        FactorySetup.EFFICIENCY_2_ITEM,
+                        FactorySetup.EFFICIENCY_3_ITEM,
+                        Blocks.REDSTONE_BLOCK,
+                        Blocks.BLAST_FURNACE),
+                new RecipePerk(
+                        FactorySetup.LOOTING_1_ITEM,
+                        FactorySetup.LOOTING_2_ITEM,
+                        FactorySetup.LOOTING_3_ITEM,
+                        Items.DIAMOND_SWORD,
+                        Blocks.LAPIS_BLOCK),
+                new RecipePerk(
+                        FactorySetup.MASS_1_ITEM,
+                        FactorySetup.MASS_2_ITEM,
+                        FactorySetup.MASS_3_ITEM,
+                        Items.MAGMA_CREAM,
+                        Blocks.NETHER_WART),
+                new RecipePerk(
+                        FactorySetup.RATE_1_ITEM,
+                        FactorySetup.RATE_2_ITEM,
+                        FactorySetup.RATE_3_ITEM,
+                        Blocks.REDSTONE_BLOCK,
+                        Blocks.REDSTONE_BLOCK),
+                new RecipePerk(
+                        FactorySetup.XP_1_ITEM,
+                        FactorySetup.XP_2_ITEM,
+                        FactorySetup.XP_3_ITEM,
+                        Blocks.ENCHANTING_TABLE,
+                        Items.LAPIS_LAZULI),
+                new RecipePerk(
+                        FactorySetup.TIER_SHARD_1_ITEM,
+                        FactorySetup.TIER_SHARD_2_ITEM,
+                        FactorySetup.TIER_SHARD_3_ITEM,
+                        Items.GOLDEN_CARROT,
+                        Items.NETHER_WART)
+        };
+
+        for (RecipePerk recipePerk : recipePerks) {
+            ShapedRecipeBuilder.shapedRecipe(recipePerk.perkItem1.get())
+                    .patternLine(" 1 ")
+                    .patternLine(" e ")
+                    .patternLine(" 2 ")
+                    .key('e', GenericSetup.ENCH_PLATE_1.get())
+                    .key('1', recipePerk.i1)
+                    .key('2', recipePerk.i2)
+                    .setGroup(Woot.MODID)
+                    .addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE))
+                    .build(consumer);
+            ShapedRecipeBuilder.shapedRecipe(recipePerk.perkItem2.get())
+                    .patternLine(" 1 ")
+                    .patternLine("e e")
+                    .patternLine(" 2 ")
+                    .key('e', GenericSetup.ENCH_PLATE_2.get())
+                    .key('1', recipePerk.i1)
+                    .key('2', recipePerk.i2)
+                    .setGroup(Woot.MODID)
+                    .addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE))
+                    .build(consumer);
+            ShapedRecipeBuilder.shapedRecipe(recipePerk.perkItem3.get())
+                    .patternLine(" 1 ")
+                    .patternLine("eee")
+                    .patternLine(" 2 ")
+                    .key('e', GenericSetup.ENCH_PLATE_3.get())
+                    .key('1', recipePerk.i1)
+                    .key('2', recipePerk.i2)
+                    .setGroup(Woot.MODID)
+                    .addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE))
+                    .build(consumer);
+        }
 
         /**
          * Other

9be8881df881bd4d4811b2615ee618164b64ab29
Ipsis <ipsis418@gmail.com>
2020-08-26 12:57:12 +0100

    data: regen data files

--- src/main/java/ipsis/woot/crafting/InfuserRecipe.java
+++ src/main/java/ipsis/woot/crafting/InfuserRecipe.java
@@ -1,8 +1,5 @@
 package ipsis.woot.crafting;
 
-import com.google.gson.JsonObject;
-import ipsis.woot.util.FluidStackHelper;
-import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,14 +11,12 @@ import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ObjectHolder;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
+
+import static ipsis.woot.crafting.InfuserRecipeBuilder.SERIALIZER;
 
 public class InfuserRecipe implements IRecipe<IInventory> {
 
@@ -64,20 +59,6 @@ public class InfuserRecipe implements IRecipe<IInventory> {
     public int getEnergy() { return this.energy; }
 
     public static final IRecipeType<InfuserRecipe> INFUSER_TYPE = IRecipeType.register("infuser");
-
-    public static InfuserRecipe infuserRecipe(ResourceLocation id, Ingredient ingredient, Ingredient augment, int augmentCount, FluidStack fluidStack, IItemProvider result, int count, int energy) {
-        return new InfuserRecipe(id, ingredient, augment, augmentCount, fluidStack, result, count, energy);
-    }
-
-    public static InfuserRecipe infuserRecipe(ResourceLocation id, Ingredient ingredient, Ingredient augment, int augmentCount, FluidStack fluidStack, IItemProvider result, int energy) {
-        return new InfuserRecipe(id, ingredient, augment, augmentCount, fluidStack, result, 1, energy);
-    }
-
-    public void build(Consumer<IFinishedRecipe> consumer, ResourceLocation id) {
-        if (this.ingredient == null || this.fluid.isEmpty())
-            throw new IllegalStateException("No valid ingredient or fluid for recipe " + id);
-        consumer.accept(new Finished(id, this.ingredient, this.augment, this.augmentCount, this.fluid, this.result, this.count, this.energy));
-    }
 
     /**
      * Jei
@@ -176,76 +157,5 @@ public class InfuserRecipe implements IRecipe<IInventory> {
     public IRecipeType<?> getType() {
         return type;
     }
-
-    /**
-     * IFinishedRecipe
-     */
-    public static class Finished implements IFinishedRecipe {
-
-        private final ResourceLocation id;
-        private final Ingredient ingredient;
-        private final Ingredient augment;
-        private final int augmentCount;
-        private final FluidStack fluid;
-        private final Item result;
-        private final int count;
-        private final int energy;
-
-
-        private Finished(ResourceLocation id, Ingredient ingredient, Ingredient augment, int augmentCount, FluidStack fluidStack, Item result, int count, int energy) {
-            this.id = id;
-            this.ingredient = ingredient;
-            this.augment = augment;
-            this.augmentCount = augmentCount;
-            this.fluid = fluidStack;
-            this.result = result;
-            this.count = count;
-            this.energy = energy;
-        }
-
-        @Override
-        public void serialize(JsonObject json) {
-            json.add("ingredient", this.ingredient.serialize());
-            if (augment != Ingredient.EMPTY) {
-                json.add("augment", this.augment.serialize());
-                if (this.augmentCount > 1)
-                    json.addProperty("augment_count", augmentCount);
-            }
-
-            json.add("infuse", FluidStackHelper.create(this.fluid));
-
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("item", ForgeRegistries.ITEMS.getKey(this.result).toString());
-            if (this.count > 1)
-                jsonObject.addProperty("count", this.count);
-            json.add("result", jsonObject);
-            json.addProperty("energy", this.energy);
-        }
-
-        @Override
-        public ResourceLocation getID() {
-            return id;
-        }
-
-        @Override
-        public IRecipeSerializer<?> getSerializer() {
-            return SERIALIZER;
-        }
-
-        @Nullable
-        @Override
-        public JsonObject getAdvancementJson() {
-            return null;
-        }
-
-        @Nullable
-        @Override
-        public ResourceLocation getAdvancementID() {
-            return null;
-        }
-    }
-
-    @ObjectHolder("woot:infuser")
-    public static final IRecipeSerializer<IRecipe<?>> SERIALIZER = null;
 
 }

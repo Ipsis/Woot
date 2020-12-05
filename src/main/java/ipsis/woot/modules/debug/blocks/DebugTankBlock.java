f59c460fc8bf436f6eb84d99abac5e1222b67379
Ipsis <ipsis418@gmail.com>
2020-12-05 13:46:18 +0000

    debug: allow fluid interaction for debug tank

--- src/main/java/ipsis/woot/modules/debug/blocks/DebugTankBlock.java
+++ src/main/java/ipsis/woot/modules/debug/blocks/DebugTankBlock.java
@@ -6,9 +6,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidUtil;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -30,6 +38,25 @@ public class DebugTankBlock extends Block implements WootDebug {
         return new DebugTankTileEntity();
     }
 
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (worldIn.isRemote)
+            return ActionResultType.SUCCESS;
+
+        if (!(worldIn.getTileEntity(pos) instanceof DebugTankTileEntity))
+            throw new IllegalStateException("Tile entity is missing");
+
+        ItemStack heldItem = player.getHeldItem(handIn);
+        if (FluidUtil.getFluidHandler(heldItem).isPresent())
+            return FluidUtil.interactWithFluidHandler(
+                    player,
+                    handIn,
+                    worldIn,
+                    pos,
+                    null) ? ActionResultType.SUCCESS : ActionResultType.FAIL;
+
+        return ActionResultType.SUCCESS;
+    }
 
     /**
      * WootDebug
